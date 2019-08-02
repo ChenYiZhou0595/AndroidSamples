f9c036a1e79f3a6685c56f02ec4df646a4d859f4
chenyz <chenyiz@postar.cn>
2019-08-02 14:31:14 +0800

    Save Code

---
+++ sqlite/src/main/java/com/chen/sqlite/MainActivity.java
@@ -0,0 +1,26 @@
+package com.chen.sqlite;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button btnCreateDatabase = findViewById(R.id.btn_create_database);
+        btnCreateDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
---
+++ sqlite/src/main/java/com/chen/sqlite/MyDatabaseHelper.java
@@ -0,0 +1,47 @@
+package com.chen.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * @description:
+ * @author: Chenyz
+ * @date: 2019/8/2 10:35
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String CREATE_BOOK = "create table Book (" +
+            "id integer primary key autoincrement, " +
+            "author text, " +
+            "price real, " +
+            "pages integer, " +
+            "name text)";
+
+    private static final String CREATE_CATEGORY = "create table Category (" +
+            "id integer primary key autoincrement, " +
+            "category_name text," +
+            "category_code integer)";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
+    }
+}
