61361b6f4aa8674590b08dd910f00ac20dbc3b9f
chenyz <chenyiz@postar.cn>
2019-08-09 15:32:51 +0800

    Save Code

---
+++ app/src/main/java/com/chen/androidsamples/StringUtils.java
@@ -0,0 +1,57 @@
+package com.chen.androidsamples;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class StringUtils {
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+
+    /**
+     * 字符串 千位符
+     *
+     * @param num
+     * @return
+     */
+    public static String num2thousand(String num) {
+        String numStr = "";
+        if (isEmpty(num)) {
+            return numStr;
+        }
+        NumberFormat nf = NumberFormat.getInstance();
+        try {
+            DecimalFormat df = new DecimalFormat("#,###");
+            numStr = df.format(nf.parse(num));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return numStr;
+    }
+
+    /**
+     * 字符串 千位符  保留两位小数点后两位
+     *
+     * @param num
+     * @return
+     */
+    public static String num2thousand00(String num) {
+        String numStr = "";
+        if (isEmpty(num)) {
+            return numStr;
+        }
+        NumberFormat nf = NumberFormat.getInstance();
+        try {
+            DecimalFormat df = new DecimalFormat("#,##0.00");
+            numStr = df.format(nf.parse(num));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return numStr;
+    }
+
+}
+
+
--- app/src/test/java/com/chen/androidsamples/ExampleUnitTest.java
+++ app/src/test/java/com/chen/androidsamples/ExampleUnitTest.java
@@ -12,6 +12,18 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        String s1 = StringUtils.num2thousand00("11");
+        String s2 = StringUtils.num2thousand00("11.29");
+        String s3 = StringUtils.num2thousand00("11.229");
+        String s4 = StringUtils.num2thousand00("123456");
+        String s5 = StringUtils.num2thousand00("12345678952.19");
+        String s6 = StringUtils.num2thousand00("1324567894213.1313245613");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s5);
+        System.out.println(s6);
     }
 }
